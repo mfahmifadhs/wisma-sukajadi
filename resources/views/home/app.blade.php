3a01fc8197527e97fbd9e0fff951653c4b56d458
mfahmifadhs <mfahmifadh2@gmail.com>
2023-07-04 08:35:39 +0700

    logo kemenkes

--- resources/views/home/app.blade.php
+++ resources/views/home/app.blade.php
@@ -49,7 +49,7 @@
                         <div class="row align-items-center">
                             <div class="logo col-lg-3 text-center text-lg-left mb-3 mb-md-5 mb-lg-0">
                                 <a class="d-block" href="index.html">
-                                    <img loading="lazy" src="{{ asset('images/main/kemenkeslogo.png') }}" alt="Wisma Sukajadi Bandung">
+                                    <img loading="lazy" src="{{ asset('images/main/logo-kemenkes.png') }}" alt="Wisma Sukajadi Bandung">
                                 </a>
                             </div><!-- logo end -->
 
