13274909791740f9744cfac7ddcf7336c01e7118
mfahmifadhs <mfahmifadh2@gmail.com>
2025-01-02 08:29:20 +0700

    Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     public function index()
     {
         $pendapatan = json_encode($this->showPendapatan());
-        $reservasi  = Reservasi::where(DB::raw("DATE_FORMAT(tanggal_reservasi, '%Y')"), Carbon::now()->format('Y'))->orderBy('tanggal_masuk', 'ASC')->get();
+        $reservasi  = Reservasi::where(DB::raw("DATE_FORMAT(tanggal_pembayaran, '%Y')"), Carbon::now()->format('Y'))->orderBy('tanggal_masuk', 'ASC')->get();
         $book       = Reservasi::where('status_reservasi', 10)->orderBy('id_reservasi', 'ASC')->get();
         return view('pages.dashboard.show', compact('reservasi','pendapatan','book'));
 
@@ -58,10 +58,10 @@ class DashboardController extends Controller
 
     public function showPendapatan()
     {
-        $result = Reservasi::select(DB::raw("(DATE_FORMAT(tanggal_reservasi, '%m')) as month"), DB::raw('sum(total_pembayaran) as pendapatan'))
+        $result = Reservasi::select(DB::raw("(DATE_FORMAT(tanggal_pembayaran, '%m')) as month"), DB::raw('sum(total_pembayaran) as pendapatan'))
                     ->groupBy('month')
                     ->where('status_reservasi', 14)
-		            ->where(DB::raw("DATE_FORMAT(tanggal_reservasi, '%Y')"), Carbon::now()->format('Y'))
+		            ->where(DB::raw("DATE_FORMAT(tanggal_pembayaran, '%Y')"), Carbon::now()->format('Y'))
                     ->get();
 
         return response()->json($result);
